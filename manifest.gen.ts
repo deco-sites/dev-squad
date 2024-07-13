aabc2537d5aedf924ba781539ac4e481717d0fce
Dayo <dayonel@hotmail.es>
2024-07-13 12:32:15 +0200

    Added some sections and pages.

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -6,13 +6,17 @@ import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Footer.tsx";
 import * as $$$$$$1 from "./sections/ItWorks.tsx";
-import * as $$$$$$2 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$2 from "./sections/Picker.tsx";
+import * as $$$$$$3 from "./sections/Team.tsx";
+import * as $$$$$$4 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Footer.tsx": $$$$$$0,
     "site/sections/ItWorks.tsx": $$$$$$1,
-    "site/sections/Theme/Theme.tsx": $$$$$$2,
+    "site/sections/Picker.tsx": $$$$$$2,
+    "site/sections/Team.tsx": $$$$$$3,
+    "site/sections/Theme/Theme.tsx": $$$$$$4,
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
