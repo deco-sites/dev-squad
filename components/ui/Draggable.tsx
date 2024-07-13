b9107c3ad3c2d70b1cc5071e0ccf72017c9f8748
Dawntraoz <albasilventefu@gmail.com>
2024-07-13 18:55:43 +0200

    fix: max container

--- components/ui/Draggable.tsx
+++ components/ui/Draggable.tsx
@@ -33,7 +33,7 @@ export default function Draggable({ members }: DraggableProps) {
       />
 
       <div
-        class="container mx-auto bg-neutral bg-opacity-5 py-8 px-4 md:p-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 rounded-lg"
+        class="container max-w-[1280px] mx-auto bg-neutral bg-opacity-5 py-8 px-4 md:p-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 rounded-lg"
         id="grid-cards"
       >
         {members && members.map((m) => {
