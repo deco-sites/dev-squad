eea7795f33bcc19486719d8d2ddf69a1ce6b78ee
Dawntraoz <albasilventefu@gmail.com>
2024-07-13 22:22:15 +0200

    fix: links missing

--- components/ui/CardMember.tsx
+++ components/ui/CardMember.tsx
@@ -38,7 +38,7 @@ export default function Members({ member, selected }: { member: Member, selected
           <span class="truncate">{member.role}</span>
         </p>
         <footer class="flex gap-x-2 lg:gap-x-4">
-          <a href={member.xUrl} aria-label={`Link to the Twitter of ${member.name}`} target="_blank">
+          {member.xUrl && (<a href={member.xUrl} aria-label={`Link to the Twitter of ${member.name}`} target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -53,8 +53,8 @@ export default function Members({ member, selected }: { member: Member, selected
             >
               <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
             </svg>
-          </a>
-          <a href={member.linkedInUrl} aria-label={`Link to the LinkedIn of ${member.name}`} target="_blank">
+          </a>)}
+          {member.linkedInUrl && (<a href={member.linkedInUrl} aria-label={`Link to the LinkedIn of ${member.name}`} target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -71,8 +71,8 @@ export default function Members({ member, selected }: { member: Member, selected
               <rect width="4" height="12" x="2" y="9" />
               <circle cx="4" cy="4" r="2" />
             </svg>
-          </a>
-          <a href={member.gitHubUrl} aria-label={`Link to the GitHub of ${member.name}`} target="_blank">
+          </a>)}
+          {member.gitHubUrl && (<a href={member.gitHubUrl} aria-label={`Link to the GitHub of ${member.name}`} target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -88,7 +88,7 @@ export default function Members({ member, selected }: { member: Member, selected
               <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
               <path d="M9 18c-4.51 2-5-2-7-2" />
             </svg>
-          </a>
+          </a>)}
           <div class="ml-auto font-josefin-sans flex gap-2 lg:gap-3">
             <svg
               class="w-4 lg:w-5"
