89aee2b03a0e450517355aefa077c8ae11d98a9a
Dawntraoz <albasilventefu@gmail.com>
2024-07-13 18:48:49 +0200

    feat: include the latest

--- components/ui/Draggable.tsx
+++ components/ui/Draggable.tsx
@@ -33,7 +33,7 @@ export default function Draggable({ members }: DraggableProps) {
       />
 
       <div
-        class="container mx-auto min-h-80 bg-neutral bg-opacity-5 p-6 md:p-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 rounded-lg"
+        class="container mx-auto bg-neutral bg-opacity-5 py-8 px-4 md:p-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 rounded-lg"
         id="grid-cards"
       >
         {members && members.map((m) => {
